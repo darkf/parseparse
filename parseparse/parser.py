f8fc0bfe4efe188780bb368e393032c3914884ba
darkf <lw9k123@gmail.com>
2017-03-20 09:52:04 -0700

    don't rely on metaparser

--- parseparse/parser.py
+++ parseparse/parser.py
@@ -1,5 +1,22 @@
-import metaparser as mp
 import re
+
+# Grammar AST
+def node(name, props):
+    def set_props(self, *propvs):
+        for prop, value in zip(props.split(), propvs):
+            setattr(self, prop, value)
+    return type(name, (Node,), {"__init__": set_props,
+                                "__repr__": lambda self: "<%s: %s>" % (name, " ".join("%s=%r" % (k,v) for k,v in self.__dict__.items())),
+                                "__eq__": lambda self, other: self.__class__.__name__ == other.__class__.__name__ and self.__dict__ == other.__dict__
+                                })
+
+class Node: pass
+Lit = node('Lit', 'v')
+Nonterminal = node('Nonterminal', 'n')
+Regex = node('Regex', 'r')
+Ident = node('Ident', 'n')
+Rule = node('Rule', 'syms tf')
+Prod = node('Prod', 'nt rules')
 
 def mkgrammar(ast):
     symtab = {}
@@ -17,15 +34,15 @@ class ParseError(Exception): pass
 
 def expected(g, p):
     # Given a grammar node, return an English description of what should be expected
-    if is_(p, mp.Prod): return " or ".join([expected(g, rule) for rule in p.rules])
-    if is_(p, mp.Rule): return expected(g, p.syms[0])
-    elif is_(p, mp.Lit): return repr(p.v)
-    elif is_(p, mp.Nonterminal): return expected(g, g[p.n])
-    elif is_(p, mp.Regex): return "/%s/" % p.r
+    if is_(p, Prod): return " or ".join([expected(g, rule) for rule in p.rules])
+    if is_(p, Rule): return expected(g, p.syms[0])
+    elif is_(p, Lit): return repr(p.v)
+    elif is_(p, Nonterminal): return expected(g, g[p.n])
+    elif is_(p, Regex): return "/%s/" % p.r
     else: raise Exception()
 
 def parse(g, p, s, n):
-    if is_(p, mp.Prod):
+    if is_(p, Prod):
         err = Exception("Parse error")
         for rule in p.rules:
             # print("Trying rule:", rule)
@@ -44,18 +61,18 @@ def parse(g, p, s, n):
                 if rule.tf: nodes = rule.tf(nodes)
                 return offset, nodes
         raise ParseError("Expected %s" % expected(g, p)) # raise err
-    elif is_(p, mp.Lit):
+    elif is_(p, Lit):
         if len(s) - n < len(p.v):
             raise ParseError("Parse error")
         r = s[n:n + len(p.v)]
         if r != p.v:
             raise ParseError("Parse error: expected '%s', got '%s'" % (p.v, r))
         return n + len(p.v), r
-    elif is_(p, mp.Regex):
+    elif is_(p, Regex):
         m = re.match(p.r, s[n:])
         if m is None: raise ParseError("Parse error: /%s/ failed to match '%s[...]'" % (p.r, s[n:n+16]))
         return n + len(m.group(0)), m.group(0)
-    elif is_(p, mp.Nonterminal):
+    elif is_(p, Nonterminal):
         return parse(g, g[p.n], s, n)
     else: raise Exception("Unhandled parse node: " + str(p))
 
@@ -65,16 +82,12 @@ def parseall(g, p, s, n):
         raise ParseError("Didn't match entire string")
     return r
 
-# test
+# metagrammar
 
 def parse_tf(tf_str):
     return tf_str.lstrip("->").strip().lstrip("{").rstrip("}")
 
-nt = mp.Nonterminal
-prod = mp.Prod
-rule = mp.Rule
-lit = mp.Lit
-regex = mp.Regex
+nt = Nonterminal
 
 def parse_tf(tf):
     code = tf.lstrip("->").strip().lstrip("{").rstrip('}')
@@ -82,54 +95,54 @@ def parse_tf(tf):
 
 bootstrap_grammar = mkgrammar([
     # S: prods -> { s[0] };
-    prod("S", [ rule([ nt("prods") ], lambda s: s[0]) ]),
+    Prod("S", [ Rule([ nt("prods") ], lambda s: s[0]) ]),
 
     # prods: prod ws prods -> { [s[0]] + s[2] }
     #      | prod -> { [s[0]] };
-    prod("prods", [
-        rule([ nt("prod"), nt("ws"), nt("prods") ], lambda s: [s[0]] + s[2]),
-        rule([ nt("prod") ], lambda s: [s[0]])
+    Prod("prods", [
+        Rule([ nt("prod"), nt("ws"), nt("prods") ], lambda s: [s[0]] + s[2]),
+        Rule([ nt("prod") ], lambda s: [s[0]])
     ]),
 
     # prod: ident ':' ws rules ws ';' ws -> { Prod(s[0], s[3]) };
-    prod("prod", [
-        rule([ nt("ident"), lit(":"), nt("ws"), nt("rules"), nt("ws"), lit(";"), nt("ws") ], lambda s: prod(s[0], s[3]))
+    Prod("prod", [
+        Rule([ nt("ident"), Lit(":"), nt("ws"), nt("rules"), nt("ws"), Lit(";"), nt("ws") ], lambda s: Prod(s[0], s[3]))
     ]),
 
     # rules: rule ws '|' ws rules -> { [s[0]] + s[4] }
     #      | rule -> { [s[0]] };
-    prod("rules", [
-        rule([ nt("rule"), nt("ws"), lit("|"), nt("ws"), nt("rules") ], lambda s: [s[0]] + s[4]),
-        rule([ nt("rule") ], lambda s: [s[0]])
+    Prod("rules", [
+        Rule([ nt("rule"), nt("ws"), Lit("|"), nt("ws"), nt("rules") ], lambda s: [s[0]] + s[4]),
+        Rule([ nt("rule") ], lambda s: [s[0]])
     ]),
 
     # rule: syms ws '-> {' /[^}]+/ '}' -> { Rule(s[0], s[3].lstrip("->").strip().lstrip("{").rstrip(chr(125))) };
     #     | syms -> { Rule(s[0], None) };
-    prod("rule", [
-        rule([ nt("syms"), nt("ws"), lit("-> {"), regex(r"[^}]+"), lit("}") ], lambda s: rule(s[0], parse_tf(s[3]))),
-        rule([ nt("syms") ], lambda s: rule(s[0], None))
+    Prod("rule", [
+        Rule([ nt("syms"), nt("ws"), Lit("-> {"), Regex(r"[^}]+"), Lit("}") ], lambda s: Rule(s[0], parse_tf(s[3]))),
+        Rule([ nt("syms") ], lambda s: Rule(s[0], None))
     ]),
 
     # syms: sym ws syms -> { [s[0]] + s[2] }
     #     | sym -> { [s[0]] }
-    prod("syms", [
-        rule([ nt("sym"), nt("ws"), nt("syms") ], lambda s: [s[0]] + s[2]),
-        rule([ nt("sym") ], lambda s: [s[0]])
+    Prod("syms", [
+        Rule([ nt("sym"), nt("ws"), nt("syms") ], lambda s: [s[0]] + s[2]),
+        Rule([ nt("sym") ], lambda s: [s[0]])
     ]),
 
     # sym: ident -> { Nonterminal(s[0]) }
     #     | /\\u002f[^\\u002f]+\\u002f/ -> { Regex(s[0][1:-1]) }
     #     | /'[^']+'/ -> { Lit(s[0][1:-1]) };
-    prod("sym", [
-        rule([ nt("ident") ], lambda s: nt(s[0])),
-        rule([ regex(r"\u002f[^\u002f]+\u002f") ], lambda s: regex(s[0][1:-1])),
-        rule([ regex(r"'[^']+'") ], lambda s: lit(s[0][1:-1]))
+    Prod("sym", [
+        Rule([ nt("ident") ], lambda s: nt(s[0])),
+        Rule([ Regex(r"\u002f[^\u002f]+\u002f") ], lambda s: Regex(s[0][1:-1])),
+        Rule([ Regex(r"'[^']+'") ], lambda s: Lit(s[0][1:-1]))
     ]),
 
     # ident: /[a-zA-Z_]+/ -> { s[0] };
     # ws: /\s*/ -> { None };
-    prod("ident", [ rule([ regex(r"[a-zA-Z_]+") ], lambda s: s[0]) ]),
-    prod("ws", [ rule([ regex(r"\s*") ], lambda s: None) ])
+    Prod("ident", [ Rule([ Regex(r"[a-zA-Z_]+") ], lambda s: s[0]) ]),
+    Prod("ws", [ Rule([ Regex(r"\s*") ], lambda s: None) ])
 ])
 
 # meta grammar
